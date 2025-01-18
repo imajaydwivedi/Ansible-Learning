16d6c4abdf6c111bd58848cc242f857db33a70f0
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2025-01-18 22:21:03 +0530

    added info to setup nodes

---
+++ ansible-core-concepts-and-advanced-features/setup-ansible-control-node/ubuntu-control-node.sh
@@ -0,0 +1,18 @@
+:<<'COMMANDS_UBUNTU'
+
+Setting up an Ubuntu Ansible Control Node
+------------------------------------------
+
+-> Install ansible
+sudo apt install ansible-core
+
+ansible --version
+
+-> install ansible-navigator
+pip install ansible-navigator
+
+
+
+COMMANDS_UBUNTU
+
+
