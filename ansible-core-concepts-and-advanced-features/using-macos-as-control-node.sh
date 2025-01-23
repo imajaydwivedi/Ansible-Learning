8fd831fb8808f55a57deacd658024336ee07812a
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2025-01-23 13:24:42 +0530

    hide gui login for ansible

--- ansible-core-concepts-and-advanced-features/using-macos-as-control-node.sh
+++ ansible-core-concepts-and-advanced-features/using-macos-as-control-node.sh
@@ -9,6 +9,15 @@ brew install ansible
 -> verify installation
 ansible --version
 
+# create ansible user
+
+# hide GUI login for user
+sudo dscl . create /Users/ansible IsHidden 1
+
+# verify ansible user
+id ansible
+ssh-copy-id ansible@localhost
+
 # Run following command without command line parameters
 ansible all -a "ls -l /root"
 
