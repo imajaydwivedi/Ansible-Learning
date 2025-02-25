320b6fc9c2d7778598aa7945f48b2c4c1f452c4d
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2025-02-25 16:37:22 +0530

    lsn10 scripts added

--- ansible-core-concepts-and-advanced-features/using-conditionals/using-conditionals.sh
+++ ansible-core-concepts-and-advanced-features/using-conditionals/using-conditionals.sh
@@ -1,5 +1,9 @@
 :<<'USING_CONDITIONALS_COMMENTS'
 
+https://spacelift.io/blog/ansible-loops
+https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_loops.html
+
+
 Understanding Loops
 ---------------------------------
 
