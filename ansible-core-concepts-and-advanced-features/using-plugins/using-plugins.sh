16f124ba2b538de9f44ef3b331a7608b6dca0fe0
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2025-02-26 19:54:30 +0530

    added scripts for lsn 11

---
+++ ansible-core-concepts-and-advanced-features/using-plugins/using-plugins.sh
@@ -0,0 +1,33 @@
+:<<'USING_PLUGINS_COMMENTS'
+
+Understanding Plugins
+----------------------------------------
+
+-> Plugins are pieces of code that augment Ansible's core functionality.
+
+-> Default plugins are provided, and custom plugins can be added easily.
+
+-> Some plugins need to be enabled in ansible.cfg.
+
+-> A filter is a plugin type that allows you to modify data.
+
+-> Filters are typically used to change variables before using them.
+
+-> As there are many filters available, these are covered separately from other plugins in next lesson
+
+
+Understanding Plugin Types
+-------------------------------------------------
+
+-> Plugins are provided to add different types of functionality:
+
+  -> Filter plugins: used to modify text
+  -> Cache plugins: plugins that allow caching of facts
+  -> Inventory plugins: plugins that allow different types of dynamic inventory
+  -> Lookup plugins: a very wide range of plugins that allow fetching data externally
+  -> Callback plugins: plugins that are used to collect usage information about Ansible
+
+
+
+USING_PLUGINS_COMMENTS
+
